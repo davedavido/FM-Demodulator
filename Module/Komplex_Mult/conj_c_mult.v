ffba46cf3e80b600b2e954d5ddcb04dfa9521284
David Lohner <Davidlohner@outlook.de>
2023-03-11 15:43:15 +0100

    *conj_c_mult added

---
+++ Module/Komplex_Mult/conj_c_mult.v
@@ -0,0 +1,63 @@
+module conj_c_mult #(parameter WIDTH = 16)   
+                (clk,      
+                rst,
+                start_i,     
+                real_i,
+				imag_i,
+                demod_o);
+
+
+/*Ein- und Ausgänge */
+input clk, rst;
+input start_i;
+input signed [WIDTH-1:0] real_i, imag_i;       // Eingangssignal
+output wire signed  [WIDTH-1:0] demod_o;   	   // Ausgangssignal
+
+/*Intern*/
+
+reg signed [WIDTH-1:0] last_in_real_r;       		// Last Input real
+reg signed [WIDTH-1:0] last_in_imag_r;       		// Last Input imag
+
+reg signed [WIDTH-1:0] real_i_r;
+reg signed [WIDTH-1:0] imag_i_r;
+
+reg signed [WIDTH-1:0] k1;
+reg signed [WIDTH-1:0] k3;
+
+reg signed [WIDTH-1:0] k1_r;
+reg signed [WIDTH-1:0] k3_r;
+
+
+always @(posedge clk) begin
+    if (rst) begin
+    last_in_real_r 		<= 0;
+	last_in_imag_r 		<= 0;
+	real_i_r 			<= 0;
+	imag_i_r 			<= 0;
+	k1_r 				<= 0;
+	k2_r 				<= 0;	
+	end
+
+    else begin
+		last_in_real_r	<= real_i_r;
+		last_in_imag_r	<= imag_i_r;
+		real_i_r		<= real_i;
+		imag_i_r		<= real_i;
+		k1_r			<= k1;
+		k3_r			<= k3;
+    end
+end
+
+always @(*) begin
+	k1 = k1_r;
+	k3 = k3_r;
+	
+    if (start_i) begin
+		k1 = real_i_r * (last_in_real_r - last_in_imag_r);    // k1 = a*(c + conj(d)) = a* (c - d)
+		k3 = last_in_real_r * (imag_i_r - real_i_r);   		  // k3 = c * (b - a)
+    end
+end
+
+assign demod_o = k1_r + k2_r;
+
+endmodule
---
+++ Module/Komplex_Mult/conj_c_mult_tb.v
@@ -0,0 +1,72 @@
+module conj_c_mult_tb;
+
+  // Parameter
+  parameter WIDTH = 16;
+
+  // Signale
+  reg               clk;
+  reg               rst;
+  reg signed [WIDTH-1:0]   real_i, imag_i;
+  wire signed [WIDTH-1:0]  demod_o;
+
+  reg start_i;
+
+  integer			fd_i, fd_o;
+
+  reg 				tmp;
+
+  // Instanz des zu testenden Moduls
+  avg_128 #(.WIDTH(WIDTH)) DUT (
+    .clk(clk),
+    .rst(rst),
+	.start_i(start_i)
+    .real_i (real_i),
+    .imag_i(imag_i),
+    .demod_o(demod_o)
+  );
+
+always
+	#1 	clk=!clk;
+	 
+initial begin
+	fd_i_real = $fopen("Real.txt", "r");
+	fd_i_imag = $fopen("Imag.txt", "r");
+	fd_o = $fopen("Output.txt", "w");
+	
+	if (fd_i_real)     $display("File was opened successfully : %0d", fd_i);
+    else      	  $display("File was NOT opened successfully : %0d", fd_i);
+	
+	if (fd_i_imag)     $display("File was opened successfully : %0d", fd_i);
+    else      	  $display("File was NOT opened successfully : %0d", fd_i);
+
+    if (fd_o)     $display("File was opened successfully : %0d", fd_o);
+    else      	  $display("File was NOT opened successfully : %0d", fd_o);
+	#10
+	clk				=	0;
+    start_i         =   0;
+	real_i			=	0;
+	imag_i			=	0;
+	tmp 			= 	0;
+	rst				=	1;
+	#10;
+	rst				=	0;
+    start_i         =   1;
+
+end		
+
+always @ (posedge clk) begin
+	if(start_i) begin
+		if (!($feof(fd_i_real))) begin
+			tmp = $fscanf(fd_i_real, "%d\n", real_i);
+			tmp = $fscanf(fd_i_imag, "%d\n", imag_i); 
+			$fwrite(fd_o, "%d\n", demod_o);
+		end else begin
+			$fclose(fd_i_real);
+			$fclose(fd_i_imag);
+			$fclose(fd_o);
+			$finish;
+		end
+end
+end
+
+endmodule
