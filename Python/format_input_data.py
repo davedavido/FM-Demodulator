21103249ce17766d26cb91234c7c208837618987
davedavido <david.lohner@stud.htwk-leipzig.de>
2023-03-12 15:20:41 +0100

    *to bytes Fromatting added

--- Python/format_input_data.py
+++ Python/format_input_data.py
@@ -9,3 +9,18 @@ fm_merged_32 = (fm_real << 16) | (fm_imag & 0xFFFF)
 print(fm_merged_32.max(), fm_merged_32.min())
 
 np.savetxt('/Users/Davidlohner/Documents/FM-Demodulator/Python/fm_merged_32.txt', fm_merged_32, fmt = '%d')
+
+#Get Bytes 31:24 First
+
+bytes = []
+for i in range (len(fm_merged_32)):
+    a0 = fm_merged_32[i] & 0xFF
+    a1 = (fm_merged_32[i]>>8) & 0xFF
+    a2 = (fm_merged_32[i]>>16) & 0xFF
+    a3 = (fm_merged_32[i]>>24) & 0xFF
+    bytes.append(a3)
+    bytes.append(a2)
+    bytes.append(a1)
+    bytes.append(a0)
+
+np.savetxt('/Users/Davidlohner/Documents/FM-Demodulator/Python/fm_bytes.txt', np.array(bytes), fmt = '%X')
